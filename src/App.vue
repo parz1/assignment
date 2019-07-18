5b5ebdc3ca548d1b846e5c00fed5e6c593ba3571
parz1 <1062134330@qq.com>
2019-07-18 12:53:05 +0800

    Modify header in axios :smile:

--- src/App.vue
+++ src/App.vue
@@ -9,54 +9,54 @@
 <script src="vue.min.js"></script>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  data(){
-    return{
-      list:[]
-    }
+  name: "App",
+  data() {
+    return {
+      list: []
+    };
   },
   methods: {
-    getData:function() {
-    //   axios.get('http://ksp.debug.online:6703/?tdsourcetag=s_pctim_aiomsg',{
-    //     headers:"Access-Control-Allow-Origin: *"
-    //   })
-    //   .then(response => {
-          
-    //       this.list[0]=response.data;
-    //       console.log(response.data);
-    //   })
-    //   .catch(response => {
-    //       console.log('error');
-    //   });
-      var instance=axios.create();
-      instance.defaults.timeout=2500;
-      instance.get('http://ksp.debug.online:6703/',{
-        timeout:5000,
-        headers:"Access-Control-Allow-Origin: *"
-      })
-      .then(response => {
-          this.list[0]=response.data;
+    getData: function() {
+      //   axios.get('http://ksp.debug.online:6703/?tdsourcetag=s_pctim_aiomsg',{
+      //     headers:"Access-Control-Allow-Origin: *"
+      //   })
+      //   .then(response => {
+
+      //       this.list[0]=response.data;
+      //       console.log(response.data);
+      //   })
+      //   .catch(response => {
+      //       console.log('error');
+      //   });
+      var instance = axios.create();
+      instance.defaults.timeout = 2500;
+      instance
+        .get("http://ksp.debug.online:6703/", {
+          timeout: 5000,
+          headers: { "Access-Control-Allow-Origin": "*" }
+        })
+        .then(response => {
+          this.list[0] = response.data;
           console.log(response.data);
-      })
-      .catch(response =>{
-        console.log('error');
-      })
+        })
+        .catch(response => {
+          console.log("error");
+        });
     }
-    
   },
   mounted() {
     this.getData();
   }
-}
+};
 </script>
 
 //document.getElementById("cheats").innerHTML="cheats:"+JSON.stringify(response.data);
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
